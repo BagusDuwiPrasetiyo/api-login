d0a6bdfc5ea334a87740418e2599cbae73cc835a
unknown <bagusduwi9@gmail.com>
2021-10-04 14:19:50 +0700

    commit

---
+++ app/Http/Controllers/api/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $check = DB::table('data_pegawai')->where('nip', $request->nip);
+
+        if ($check->count() == 0) {
+            $resp = [
+                'metadata' => [
+                    'code' => 400,
+                    'status' => ''
+                ]
+            ];
+
+            return response()->json($resp);
+        }
+
+        if (!$check->get()[0]->password == Crypt::encrypt($request->password)) {
+            $resp = [
+                'metadata' => [
+                    'code' => 400,
+                    'status' => ''
+                ]
+            ];
+
+            return response()->json($resp);
+        }
+
+        return true;
+    }
+}
